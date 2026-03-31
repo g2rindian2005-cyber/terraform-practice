4c84ed23edb089899725252b01bd1fccc85fe87e
gokul <gokul9356782322@gmail.com>
2026-03-31 12:12:20 +0530

    ipmort

--- Day-2-Terraform-all-config-files/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "my-ec2" { 
-    ami = var.ami_id
-  instance_type = var.instance_type
-  tags = {
-    Name        = "gokul-ec2-account2"
-    Environment = "Account2"
-  }
-}
--- day-6-statelacking/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" { 
-    bucket = "rathod-gokul01"
-    key    = "jawan-folder/terraform.tf"
-    region = "us-east-1"
-    dynamodb_table = "terraform-state-lock"
-  }
-}
